9be409a1a2a4567ef6631e1c67898db0bde0bc48
karthik gurnani <118505109+karthik393@users.noreply.github.com>
2023-11-05 14:07:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i<numberOfDrumButtons; i++) {
+
+   document.querySelectorAll(".drum")[i].addEventListener("click" , function() {
+
+    let buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    console.log(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+  });
+
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key, "keypress")
+
+  buttonAnimation(event.key);
+});
+
+   
+
+function makeSound(key) {
+
+   switch (key) {
+      case "w":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      console.log(key)
+      break;
+
+      case "a":
+      let kick = new Audio("sounds/bass-kick.mp3.mp3");
+      kick.play();
+      console.log(key)      
+      break;
+  
+      case "s":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      console.log(key)
+      break;
+
+      case "d":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      console.log(key)
+      break;
+
+      case "j":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      console.log(key)
+      break;
+
+      case "k":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      console.log(key)
+      break;
+
+      case "l":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      console.log(key)
+      break;
+
+
+      default: console.log(key);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+   let activeButton = document.querySelector("." + currentKey);
+
+   activeButton.classList.add("pressed");
+
+   setTimeout(function() {
+     activeButton.classList.remove("pressed");
+   }, 120)
+}
